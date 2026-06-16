57d9c016ec86934c609263dc5573b0e45ecc365d
Supreeth kumar <jsupreeth2005@gmail.com>
2026-06-16 11:18:14 +0530

    fix: allow CORS preflight requests from github.io pages

--- app/main.py
+++ app/main.py
@@ -23,11 +23,16 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://harishbalaji826-ops.github.io",
+    "https://supreethkumarjagarlamudi.github.io",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex="https://.*\.github\.io",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
